d5fcb29bbf9ee45b17318b87e032251e69bb2341
hoseabbpr <hosea@breadandbutterpr.com>
2019-10-25 16:54:06 -0400

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,45 @@
+import { NgModule }       from '@angular/core';
+import { BrowserModule }  from '@angular/platform-browser';
+import { FormsModule }    from '@angular/forms';
+import { HttpClientModule }    from '@angular/common/http';
+
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+// import { InMemoryDataService }  from './in-memory-data.service';
+
+import { AppRoutingModule }     from './app-routing.module';
+
+import { AppComponent }         from './app.component';
+
+
+import { PLATFORM_ID, APP_ID, Inject } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { HomeComponent } from './home/home.component';
+
+
+
+@NgModule({
+  imports: [
+    BrowserModule.withServerTransition({ appId: 'tour-of-heroes' }),
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    // HttpClientInMemoryWebApiModule.forRoot(
+    //   InMemoryDataService, { dataEncapsulation: false }
+    // )
+  ],
+  declarations: [
+    AppComponent,
+    HomeComponent
+  ],
+  providers: [],
+  bootstrap: [ AppComponent ]
+})
+export class AppModule {
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: Object,
+    @Inject(APP_ID) private appId: string) {
+    const platform = isPlatformBrowser(platformId) ?
+      'in the browser' : 'on the server';
+    console.log(`Running ${platform} with appId=${appId}`);
+  }
+}
